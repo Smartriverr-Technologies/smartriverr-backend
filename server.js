18dd5bd8487e462c595f24eb2479341666c7aa35
Blackrayy <info@blackrayy.com>
2025-07-30 17:17:19 +0530

    Clean commit without secrets

--- server.js
+++ server.js
@@ -58,7 +58,20 @@ app.use(cors());
 
 // 4. Firebase Admin SDK Initialization
 // Get the service account key you downloaded from Firebase
-const serviceAccount = require('./serviceAccountKey.json');
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.project_id,
+  private_key: process.env.private_key.replace(/\\n/g, '\n'),
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain
+};
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
